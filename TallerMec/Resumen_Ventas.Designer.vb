0336a7289eb13d058e002904fc6608507e7d87c0
JP-SOMBRA <pablo.hernandez.censa@gmail.com>
2025-11-13 18:36:13 -0300

    Avance historial ventas

---
+++ TallerMec/Resumen_Ventas.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Resumen_Ventas
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Resumen_Ventas))
+        PictureBox1 = New PictureBox()
+        GroupBox1 = New GroupBox()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        DataGridView1 = New DataGridView()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBox1.SuspendLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(-4, -3)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(1197, 730)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 14
+        PictureBox1.TabStop = False
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(DataGridView1)
+        GroupBox1.Controls.Add(Button2)
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Location = New Point(246, 92)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(666, 507)
+        GroupBox1.TabIndex = 16
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Historial de Ventas:"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(18, 126)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(232, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Introduzca el ID de la venta:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(256, 126)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(135, 31)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.LawnGreen
+        Button1.Location = New Point(407, 180)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(101, 38)
+        Button1.TabIndex = 2
+        Button1.Text = "Buscar"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.DarkOrange
+        Button2.Location = New Point(539, 179)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(106, 38)
+        Button2.TabIndex = 3
+        Button2.Text = "Ver todo"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(90, 251)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 62
+        DataGridView1.Size = New Size(486, 225)
+        DataGridView1.TabIndex = 4
+        ' 
+        ' Resumen_Ventas
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1193, 661)
+        Controls.Add(GroupBox1)
+        Controls.Add(PictureBox1)
+        Name = "Resumen_Ventas"
+        Text = "Resumen_Ventas"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ TallerMec/Resumen_Ventas.vb
@@ -0,0 +1,9 @@
+﻿Public Class Resumen_Ventas
+    Private Sub Resumen_Ventas_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
